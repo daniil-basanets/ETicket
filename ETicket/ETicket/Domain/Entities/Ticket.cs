646375603a84bfeda0b12d8b3f297924ff919960
daniil-basanets <daniil.basanets@gmail.com>
2020-04-02 21:43:16 +0300

    Add Ticket model; TicketRepository

---
+++ ETicket/ETicket/Domain/Entities/Ticket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ETicket.Domain.Entities
+{
+    public class Ticket
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [ForeignKey("TicketTypeId")]
+        public TicketType TicketTypeID { get; set; }
+
+        [Required]
+        public DateTime CreatedUTCDate { get; set; }
+
+        public DateTime? ActivatedUTCDate { get; set; }
+
+        [Required]
+        public DateTime ExpirationUTCDate { get; set; }
+
+        public long? UserID { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        [Required]
+        public int TransactionHistoryId { get; set; }
+
+        [ForeignKey("TransactionHistoryId")]
+        public TransactionHistory TransactionHistory { get; set; }
+    }
+}
---
+++ ETicket/ETicket/Domain/Entities/TicketRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using ETicket.Domain.Interfaces;
+
+namespace ETicket.Domain.Entities
+{
+    public class TicketRepository : IRepository<Ticket>
+    {
+        private readonly ETicketDataContext eTicketDataContext;
+
+        public TicketRepository(ETicketDataContext eTicketDataContext)
+        {
+            this.eTicketDataContext = eTicketDataContext;
+        }
+
+        public void Create(Ticket item)
+        {// TODO CreateTicket
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {// TODO DeleteTicket
+            throw new NotImplementedException();
+        }
+
+        public Ticket Get(int id)
+        {//TODO GetTicket
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Ticket> GetAll()
+        {//TODO IEnumerable<Ticket>
+            throw new NotImplementedException();
+        }
+
+        public void Update(Ticket item)
+        {// TODO UpdateTicket
+            throw new NotImplementedException();
+        }
+    }
+}
