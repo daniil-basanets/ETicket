cbf6e644d49cd027997e96204bb8f87a8896fb65
Daria Nazarenko <daryan15646@gmail.com>
2020-04-03 11:57:09 +0300

    edit user

--- ETicket/ETicket/Domain/Entities/User.cs
+++ ETicket/ETicket/Domain/Entities/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,11 +20,19 @@ namespace ETicket.Domain.Entities
         [Required]
         [MaxLength(13)]
         public string Phone { get; set; }
-        public int RoleID { get; set; }
-        public virtual Role Role { get; set; }
-        public  int PrivilegieID { get; set; }
-        public virtual Privilegie Privilegie { get; set; }
-        public virtual int DocumentID { get; set; }
-        public virtual Document Document { get; set; }
+        [ForeignKey("RoleId")]
+        public Role Role { get; set; }
+        [ForeignKey("PrivilegieID")]
+        public Privilegie Privilegie { get; set; }
+        [ForeignKey("DocumentID")]
+        public Document Document { get; set; }
+
+
+        //public int RoleID { get; set; }
+        //public virtual Role Role { get; set; }
+        //public  int PrivilegieID { get; set; }
+        //public virtual Privilegie Privilegie { get; set; }
+        //public virtual int DocumentID { get; set; }
+        //public virtual Document Document { get; set; }
     }
 }
