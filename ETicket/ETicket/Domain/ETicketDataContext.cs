6130f76296986362de0bd5e72fddf12530da7c4b
Daria Nazarenko <daryan15646@gmail.com>
2020-04-03 11:10:40 +0300

    edit context

--- ETicket/ETicket/Domain/ETicketDataContext.cs
+++ ETicket/ETicket/Domain/ETicketDataContext.cs
@@ -9,6 +9,7 @@ namespace ETicket.Domain
         public DbSet<Role> Roles { get; set; }
         public DbSet<TicketType> TicketTypes { get; set; }
         public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public ETicketDataContext(DbContextOptions<ETicketDataContext> options) : base(options) { }
 
