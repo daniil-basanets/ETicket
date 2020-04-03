1bccdf7a50d3f1dad3d8fd6f0c3ef030f0df8fa2
dp187net1 <dp187net1@gmail.com>
2020-04-03 12:08:17 +0300

    Privilege and PrivilegeRepository added

---
+++ ETicket/ETicket/Domain/Entities/Privilege.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ETicket.Domain.Entities
+{
+	public class Privilege
+	{
+		[Key]
+		public int ID { get; set; }
+
+		[Required]
+		[MaxLength(50)]
+		public string Name { get; set; }
+
+		[Required]
+		public float Coefficient { get; set; }
+
+	}
+}
---
+++ ETicket/ETicket/Domain/Entities/PrivilegeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using ETicket.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETicket.Domain.Entities
+{
+	public class PrivilegyRepository: IRepository<Privilege>
+	{
+        #region
+
+        private readonly ETicketDataContext context;
+
+        #endregion
+
+
+    }
+}
