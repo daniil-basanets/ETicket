1e87a9ddce5c6c5b734f689dc07ce5bd054b957a
dp187net1 <dp187net1@gmail.com>
2020-04-07 22:45:46 +0300

    Delete unnecessary comments in PrivilegesController Change label "Privilege" in the left menu (_Layout.cshtml file)

--- ETicket/ETicketAdmin/Controllers/PrivilegesController.cs
+++ ETicket/ETicketAdmin/Controllers/PrivilegesController.cs
@@ -54,8 +54,6 @@ namespace ETicketAdmin.Controllers
         }
 
         // POST: Privileges/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Coefficient")] Privilege privilege)
@@ -86,8 +84,6 @@ namespace ETicketAdmin.Controllers
         }
 
         // POST: Privileges/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Coefficient")] Privilege privilege)
