d82e5414b5250373a943f58253aceeebd1f7e40f
dp187net1 <dp187net1@gmail.com>
2020-04-12 14:36:36 +0300

    Add filter to User Index

---
+++ ETicket/ETicketAdmin/wwwroot/js/user-script.js
@@ -0,0 +1,23 @@
+﻿var today = new Date();
+var day, month;
+
+if (today.getDate() <= 9)
+{
+    day = 0 + String(today.getDate());
+}
+else
+{
+    day = today.getDate();
+}
+
+if (today.getMonth()<=9)
+{
+    month = 0 + String((today.getMonth() + 1));
+}
+else
+{
+    month = today.getMonth() + 1
+}
+document.getElementById("user-birth-date").setAttribute("max", today.getFullYear() + '-' + month + '-' + day);
+document.getElementById("user-birth-date").setAttribute("value", today.getFullYear() + '-' + month + '-' + day);
+
