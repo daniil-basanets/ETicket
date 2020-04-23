fe8463d403a7c3f931f19761ce786850ef3ac46e
Anton Kalashnikov <kalash@softserve.com>
2020-04-23 14:42:43 +0300

    Add TicketTypeService.cs

--- ETicket/ETicket.ApplicationServices/Services/MapperService.cs
+++ ETicket/ETicket.ApplicationServices/Services/MapperService.cs
@@ -18,6 +18,8 @@ namespace ETicket.ApplicationServices.Services
             var config = new MapperConfiguration(cfg => {
                 cfg.CreateMap<DocumentTypeDto, DocumentType>();
                 cfg.CreateMap<DocumentDto, Document>();
+                cfg.CreateMap<TicketTypeDto, TicketType>();
+                
             });
 
             var mapper = config.CreateMapper();
