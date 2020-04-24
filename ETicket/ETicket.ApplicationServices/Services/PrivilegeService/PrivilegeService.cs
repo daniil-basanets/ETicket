722b6f501e311f931dfa582ab0cd60318f94dc2b
dp187net1 <dp187net1@gmail.com>
2020-04-24 12:17:21 +0000

    Realize IPrivilegeService by PrivilegeService

--- ETicket/ETicket.ApplicationServices/Services/PrivilegeService/PrivilegeService.cs
+++ ETicket/ETicket.ApplicationServices/Services/PrivilegeService/PrivilegeService.cs
@@ -6,7 +6,7 @@ using ETicket.DataAccess.Domain.Interfaces;
 
 namespace ETicket.ApplicationServices.Services.PrivilegeService
 {
-    class PrivilegeService
+    class PrivilegeService: IPrivilegeService
     {
         #region private members
 
