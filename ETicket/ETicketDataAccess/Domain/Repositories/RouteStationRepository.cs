7b5f781a10d460f0d30473e4a1a5e646b16a1fce
daniil-basanets <daniil.basanets@gmail.com>
2020-04-26 13:49:19 +0300

    Add RouteStation Entity models and repository

---
+++ ETicket/ETicketDataAccess/Domain/Repositories/RouteStationRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ETicket.DataAccess.Domain.Entities;
+using ETicket.DataAccess.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETicket.DataAccess.Domain.Repositories
+{
+    public class RouteStationRepository : IRepository<RouteStation, int>
+    {
+        #region Private members
+
+        private readonly ETicketDataContext context;
+
+        #endregion
+
+        public RouteStationRepository(ETicketDataContext eTicketDataContext)
+        {
+            this.context = eTicketDataContext;
+        }
+
+        public void Create(RouteStation item)
+        {
+            context.RouteStations.Add(item);
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException("Do not use this. Use DeleteStationFromRoutes or DeleteRouteFromStations instead");
+        }
+
+        public void DeleteRouteFromStations(int routeId)
+        {
+            var routeStationList = context.RouteStations.Where(m => m.RouteId == routeId);
+
+            if (routeStationList != null)
+            {
+                context.RemoveRange(routeStationList);
+            }
+        }
+
+        public void DeleteStationFromRoutes(int stationId)
+        {
+            var routeStationList = context.RouteStations.Where(m => m.RouteId == stationId);
+
+            if (routeStationList != null)
+            {
+                context.RemoveRange(routeStationList);
+            }
+        }
+
+        public RouteStation Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<RouteStation> GetAll()
+        {
+            return context.RouteStations.Include(r => r.Route)
+                .Include(s => s.Station);
+        }
+
+        public void Update(RouteStation item)
+        {
+            context.Entry(item).State = EntityState.Modified;
+        }
+    }
+}
