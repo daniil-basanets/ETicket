f2eb137d5a9a3ad32317b6e4194decec64ec045d
AleksandrKar7 <AleksandrKar7@gmail.com>
2020-05-14 20:21:32 +0300

    Return carrier in startap

--- ETicket/ETicketAdmin/Startup.cs
+++ ETicket/ETicketAdmin/Startup.cs
@@ -63,6 +63,7 @@ namespace ETicket.Admin
             services.AddTransient<IUserService, UserService>();
             services.AddTransient<IDocumentTypesService, DocumentTypesService>();
             services.AddTransient<ITicketTypeService, TicketTypeService>();
+            services.AddTransient<ICarrierService, CarrierService>();
             services.AddTransient<IPrivilegeService, PrivilegeService>();
 
 
