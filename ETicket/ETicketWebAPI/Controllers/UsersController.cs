4f89497cd4883ed8c903effa697c60b1538e04a1
Daria Nazarenko <daryan15646@gmail.com>
2020-05-15 17:36:13 +0300

    Edit user API controller

--- ETicket/ETicketWebAPI/Controllers/UsersController.cs
+++ ETicket/ETicketWebAPI/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using log4net;
 using ETicket.ApplicationServices.Services.Interfaces;
+using ETicket.ApplicationServices.DTOs;
 
 namespace ETicket.WebAPI.Controllers
 {
@@ -38,6 +39,80 @@ namespace ETicket.WebAPI.Controllers
                 var tickets = ticketService.GetTicketsByUserId(userId);
 
                 return Ok(tickets);
+            }
+            catch (Exception e)
+            {
+                log.Error(e);
+
+                return BadRequest();
+            }
+        }
+
+        // GET: api/users/5
+        [HttpGet("{id}")]
+        public IActionResult GetUser(Guid id)
+        {
+            log.Info(nameof(UsersController.GetUser));
+
+            try
+            {
+                var user = userService.GetUserById(id);
+
+                if (user == null)
+                {
+                    log.Warn(nameof(UsersController.GetUser) + " user is null");
+
+                    return NotFound();
+                }
+
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                log.Error(e);
+
+                return BadRequest();
+            }
+        }
+
+        // PUT: api/users/5
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(Guid id, UserDto userDto)
+        {
+            log.Info(nameof(UsersController.UpdateUser));
+
+            try
+            {
+                if (id != userDto.Id)
+                {
+                    log.Warn(nameof(UsersController.UpdateUser) + " id is not equal to userDto.Id");
+
+                    return BadRequest();
+                }
+
+                userService.Update(userDto);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                log.Error(e);
+
+                return BadRequest();
+            }
+        }
+
+        // POST: api/users
+        [HttpPost]
+        public IActionResult CreateUser(UserDto userDto)
+        {
+            log.Info(nameof(UsersController.CreateUser));
+
+            try
+            {
+                userService.CreateUser(userDto);
+
+                return Created(nameof(UsersController.GetUser), userDto);
             }
             catch (Exception e)
             {
