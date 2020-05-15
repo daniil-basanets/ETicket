67ab975de7f49a8ee5f1b0aab249d50f80cbb2cd
dp187net1 <dp187net1@gmail.com>
2020-05-15 19:25:11 +0300

    Add Privilege API-controller

---
+++ ETicket/ETicketWebAPI/Controllers/PrivilegeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection;
+using ETicket.ApplicationServices.Services.PrivilegeService;
+using log4net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicket.WebAPI.Controllers
+{
+    [Route("api/privileges")]
+    [ApiController]
+    public class PrivilegesController : ControllerBase
+    {
+        private readonly IPrivilegeService privilegeService;
+        private static readonly ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public PrivilegesController(IPrivilegeService privilegeService)
+        {
+            this.privilegeService = privilegeService;
+        }
+
+        [HttpGet]
+        public IActionResult GetPrivileges()
+        {
+            logger.Info(nameof(PrivilegesController));
+
+            try
+            {
+                return Ok(privilegeService.GetAll());
+            }
+            catch (Exception exception)
+            {
+                logger.Error(exception);
+
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPrivilege(int id)
+        {
+            logger.Info(nameof(PrivilegesController));
+
+            try
+            {
+                return Ok(privilegeService.Get(id));
+            }
+            catch (Exception exception)
+            {
+                logger.Error(exception);
+
+                return BadRequest();
+            }
+        }
+    }
+
+}
