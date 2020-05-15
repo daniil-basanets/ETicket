f7947596f2d22d932fc27f1e9dcff171eab4e5b7
AleksandrKar7 <AleksandrKar7@gmail.com>
2020-05-15 16:55:44 +0300

    Change Web API route for DocumentTypesController

--- ETicket/ETicketWebAPI/Controllers/DocumentTypesController.cs
+++ ETicket/ETicketWebAPI/Controllers/DocumentTypesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Http;
 
 namespace ETicket.WebAPI.Controllers
 {
-    [Route("api/documenttypes")]
+    [Route("api/document-types")]
     [ApiController]
     public class DocumentTypesController : ControllerBase
     {
