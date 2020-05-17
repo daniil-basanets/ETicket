6f09000ce9a5c2e3bb5bef2e545873d915b58750
daniil-basanets <daniil.basanets@gmail.com>
2020-05-17 22:13:38 +0300

    Refactoring chart-bar-tickets-by-types.js

--- ETicket/ETicketAdmin/wwwroot/js/chart/chart-bar-tickets-by-types.js
+++ ETicket/ETicketAdmin/wwwroot/js/chart/chart-bar-tickets-by-types.js
@@ -2,10 +2,37 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
+function number_format(number, decimals, dec_point, thousands_sep) {
+    // *     example: number_format(1234.56, 2, ',', ' ');
+    // *     return: '1 234,56'
+    number = (number + '').replace(',', '').replace(' ', '');
+    var n = !isFinite(+number) ? 0 : +number,
+        prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+        sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+        dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+        s = '',
+        toFixedFix = function (n, prec) {
+            var k = Math.pow(10, prec);
+            return '' + Math.round(n * k) / k;
+        };
+    // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+    if (s[0].length > 3) {
+        s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+    }
+    if ((s[1] || '').length < prec) {
+        s[1] = s[1] || '';
+        s[1] += new Array(prec - s[1].length + 1).join('0');
+    }
+    return s.join(dec);
+}
+
+var chartData;
+
 var actionUrl = '/metrics/GetTicketsByTicketTypes';
 $.getJSON(actionUrl, function (response) {
     if (response != null) {
-        var chartData = response;
+        chartData = response;
         var maxY = Math.max.apply(null, chartData.data);
 
         var ctx = document.getElementById("tickets-by-types");
@@ -15,13 +42,89 @@ $.getJSON(actionUrl, function (response) {
                 labels: chartData.labels,
                 datasets: [{
                     label: "Ticket count",
-                    backgroundColor: "#4e73df",
+                    backgroundColor: "rgba(78, 115, 223, 0.9)", //"#4e73df",
+                    //backgroundColor: ["rgba(0,0,128, 1)", "rgba(0,0,255, 1)",
+                    //    "rgba(0,128,0, 1)", "rgba(0,128,128, 0.2)", "rgba(0,255,0, 1)",
+                    //    "rgba(0,255,255, 1)", "rgb(128,0,0)"
+                    //],
                     hoverBackgroundColor: "#2e59d9",
                     borderColor: "#4e73df",
                     data: chartData.data,
                 }],
             },
-            options: {}
+
+            options: {
+                layout: {
+                    padding: {
+                        left: 10,
+                        right: 25,
+                        top: 25,
+                        bottom: 0
+                    }
+                },
+                scales: {
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'ticket types'
+                        },
+                        time: {
+                            unit: 'month'
+                        },
+                        gridLines: {
+                            display: false,
+                            drawBorder: false
+                        },
+                        ticks: {
+                            maxTicksLimit: 50
+                        },
+                        maxBarThickness: 25,
+                    }],
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'tickets count'
+                        },
+                        ticks: {
+                            min: 0,
+                            maxTicksLimit: 100,
+                            padding: 10,
+                            callback: function (value, index, values) {
+                                return number_format(value);
+                            }
+                        },
+                        gridLines: {
+                            color: "rgb(234, 236, 244)",
+                            zeroLineColor: "rgb(234, 236, 244)",
+                            drawBorder: false,
+                            borderDash: [2],
+                            zeroLineBorderDash: [2]
+                        }
+                    }],
+                },
+                legend: {
+                    display: false
+                },
+                tooltips: {
+                    titleMarginBottom: 10,
+                    titleFontColor: '#6e707e',
+                    titleFontSize: 14,
+                    backgroundColor: "rgb(255,255,255)",
+                    bodyFontColor: "#858796",
+                    borderColor: '#dddfeb',
+                    borderWidth: 1,
+                    xPadding: 15,
+                    yPadding: 15,
+                    displayColors: false,
+                    caretPadding: 10,
+                    callbacks: {
+                        label: function (tooltipItem, chart) {
+                            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                            return datasetLabel + ': ' + number_format(tooltipItem.xLabel);
+                        }
+                    }
+                },
+            }
         });
 
     }
