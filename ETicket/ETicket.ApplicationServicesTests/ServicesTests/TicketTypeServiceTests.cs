d73147cdfc4f395c3937aea365491b8c3d0317df
Anton Kalashnikov <kalash@softserve.com>
2020-05-17 19:13:57 +0300

    Add TicketTypeValidatorTests and refactoring TicketTypeServiceTests

--- ETicket/ETicket.ApplicationServicesTests/ServicesTests/TicketTypeServiceTests.cs
+++ ETicket/ETicket.ApplicationServicesTests/ServicesTests/TicketTypeServiceTests.cs
@@ -2,11 +2,8 @@ using System.Collections.Generic;
 using System.Linq;
 using ETicket.ApplicationServices.DTOs;
 using ETicket.ApplicationServices.Services;
-using ETicket.DataAccess.Domain;
 using ETicket.DataAccess.Domain.Entities;
 using ETicket.DataAccess.Domain.Interfaces;
-using ETicket.DataAccess.Domain.Repositories;
-using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
 
@@ -14,26 +11,28 @@ namespace ETicket.ApplicationServicesTests.ServicesTests
 {
     public class TicketTypeServiceTests
     {
+        #region Private fields
+
         private readonly TicketTypeService ticketTypeService;
         private readonly IList<TicketType> fakeTicketTypes;
         private readonly TicketTypeDto ticketTypeDto;
         private TicketType ticketType;
 
+        #endregion
+
+        #region Constructor
+
         public TicketTypeServiceTests()
         {
-            var mockRepository = new Mock<IRepository<TicketType,int>>();
+            var mockRepository = new Mock<IRepository<TicketType, int>>();
             var mockUnitOfWork = new Mock<IUnitOfWork>();
-            var mockDbSet = new Mock<DbSet<TicketType>>();
-            var context = new Mock<ETicketDataContext>();
-            
-            
 
             fakeTicketTypes = new List<TicketType>
             {
-                new TicketType {Id = 1, TypeName = "Test1", Coefficient = 2.3M, DurationHours = 12, IsPersonal = true},
-                new TicketType {Id = 2, TypeName = "Test2", Coefficient = 2.4M, DurationHours = 13, IsPersonal = false}
+                new TicketType {Id = 5, TypeName = "Test1", Coefficient = 2.3M, DurationHours = 12, IsPersonal = true},
+                new TicketType {Id = 7, TypeName = "Test2", Coefficient = 2.4M, DurationHours = 13, IsPersonal = false}
             };
-            
+
             ticketTypeDto = new TicketTypeDto
             {
                 Id = 3,
@@ -42,70 +41,110 @@ namespace ETicket.ApplicationServicesTests.ServicesTests
                 DurationHours = 16,
                 IsPersonal = false
             };
-            
-            mockDbSet.Setup(m => m.Remove(It.IsAny<TicketType>()))
-                .Callback<TicketType>(entity => fakeTicketTypes.Remove(entity));
-
-            context.Setup(c => c.TicketTypes).Returns(mockDbSet.Object);
-            context.Setup(c=>c.TicketTypes.Remove(It.IsAny<TicketType>()))
-                .Callback<TicketType>(entity => fakeTicketTypes.Remove(entity));
-            context.Setup(c => c.TicketTypes.Find(It.IsAny<int>()))
-                .Returns<TicketType>(ticketType => fakeTicketTypes[It.IsAny<int>()]);
-            
-            var repository = new TicketTypeRepository(context.Object);
 
             mockRepository.Setup(m => m.GetAll()).Returns(fakeTicketTypes.AsQueryable);
-            mockRepository.Setup(m => m.Get(fakeTicketTypes.First().Id))
-                .Returns(fakeTicketTypes.First);
+            mockRepository.Setup(m => m.Get(It.IsAny<int>()))
+                .Returns<int>(id => fakeTicketTypes.Single(t => t.Id == id));
             mockRepository.Setup(r => r.Create(It.IsAny<TicketType>()))
-                .Callback<TicketType>(x => ticketType = x);
+                .Callback<TicketType>(t => fakeTicketTypes.Add(ticketType = t));
             mockRepository.Setup(r => r.Update(It.IsAny<TicketType>()))
-                .Callback<TicketType>(x => ticketType = x);
-            // mockRepository.Setup(x=>x.Delete(It.IsAny<int>()))
-            //     .Callback<int>()
+                .Callback<TicketType>(t => ticketType = t);
+            mockRepository.Setup(x => x.Delete(It.IsAny<int>()))
+                .Callback<int>(id => fakeTicketTypes.Remove(fakeTicketTypes.Single(t => t.Id == id)));
 
-            //mockUnitOfWork.Setup(m => m.TicketTypes).Returns(mockRepository.Object);
-            mockUnitOfWork.Setup(m => m.TicketTypes).Returns(repository);
+            mockUnitOfWork.Setup(m => m.TicketTypes).Returns(mockRepository.Object);
+
             ticketTypeService = new TicketTypeService(mockUnitOfWork.Object);
         }
+
+        #endregion
+
+        [Fact]
+        public void GetTicketTypes_CheckNull_ShouldBeNotNull()
+        {
+            var actual = ticketTypeService.GetTicketTypes();
+            
+            Assert.NotNull(actual);
+        }
         
         [Fact]
-        public void GetAll_TicketTypes_ShouldCorrect()
+        public void GetTicketTypes_CompareCount_ShouldBeEqual()
         {
             var ticketTypes = ticketTypeService.GetTicketTypes();
+
+            var expected = fakeTicketTypes.Count;
+            var actual = ticketTypes.Count();
             
-            Assert.Equal(fakeTicketTypes.Count(),ticketTypes.Count());
+            Assert.Equal(expected,actual);
         }
         
-        [Fact]
-        public void GetByIdTicketType_ShouldCorrect()
+        [Theory]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void GetByIdTicketType_CheckDurationHoursInReceivedObject_ShouldBeTheSameAsInFake(int id)
         {
-            Assert.Equal(fakeTicketTypes.First().DurationHours, ticketTypeService.GetTicketTypeById(1).DurationHours);
+            var expected = fakeTicketTypes.Single(t => t.Id == id).DurationHours;
+            var actual = ticketTypeService.GetTicketTypeById(id).DurationHours;
+            
+            Assert.Equal(expected,actual);
         }
         
         [Fact]
-        public void Delete_TicketType_ShouldCorrect() 
+        public void Delete_TicketType_CountShouldDecrease()
         {
-            ticketTypeService.Delete(1);
-            Assert.Equal(1,fakeTicketTypes.Count);
+            var expected = fakeTicketTypes.Count - 1;
+            
+            ticketTypeService.Delete(fakeTicketTypes.First().Id);
+
+            var actual = fakeTicketTypes.Count;
+            
+            Assert.Equal(expected,actual);
         }
         
         [Fact]
-        public void Create_TicketType_ShouldCorrect()
+        public void Create_TicketType_ShouldBeNotNull()
         {
             ticketTypeService.Create(ticketTypeDto);
 
-            Assert.NotNull(ticketType);
-            Assert.Equal(ticketTypeDto.TypeName, ticketType.TypeName);
+            var actual = ticketType;
+            
+            Assert.NotNull(actual);
         }
         
         [Fact]
-        public void Update_TicketType_ShouldCorrect()
+        public void Create_CheckNameInNewObject_ShouldBeTheSameAsInFake()
+        {
+            ticketTypeService.Create(ticketTypeDto);
+
+            var expected = ticketTypeDto.TypeName;
+            var actual = ticketType.TypeName;
+            
+            Assert.Equal(expected, actual);
+        }
+        
+        [Fact]
+        public void Create_AddNewObject_CountShouldIncrease()
+        {
+            var expected = fakeTicketTypes.Count + 1;
+            
+            ticketTypeService.Create(ticketTypeDto);
+
+            var actual = fakeTicketTypes.Count;
+            
+            Assert.Equal(expected,actual);
+        }
+        
+        [Fact]
+        public void Update_TicketType_NameShouldBeEqualDTOsName()
         {
             ticketTypeDto.TypeName = "UpdatedName";
+            var expected = ticketTypeDto.TypeName;
+            
             ticketTypeService.Update(ticketTypeDto);
 
-            Assert.Equal(ticketTypeDto.TypeName,ticketType.TypeName);
+            var actual = ticketType.TypeName;
+
+            Assert.Equal(expected,actual);
         }
     }
 }
