289ca28642d2535bd41423fd7037f7b0a7ae14e6
Anton Kalashnikov <kalash@softserve.com>
2020-05-24 12:42:08 +0300

    Add map to area without markers

--- ETicket/ETicketAdmin/wwwroot/js/google-map-station-display.js
+++ ETicket/ETicketAdmin/wwwroot/js/google-map-station-display.js
@@ -1,20 +1,17 @@
+
 function initMap(z,y) {
-    // The location of Uluru
     var uluru = {lat: z, lng: y};
-    // The map, centered at Uluru
     var x = document.getElementById("GeoLocation");
 
+    var map = new google.maps.Map(
+        document.getElementById('GeoLocation'), {zoom: 12, center: uluru});
+    // The marker, positioned at Uluru
+    
+    new google.maps.Marker({position: uluru, map: map});
+    
     if (x.style.display === "none") {
         x.style.display = "block";
-        var map = new google.maps.Map(
-            document.getElementById('GeoLocation'), {zoom: 12, center: uluru});
-        // The marker, positioned at Uluru
-        var marker = new google.maps.Marker({position: uluru, map: map});
     } else {
         x.style.display = "none";
-        var map = new google.maps.Map(
-            document.getElementById('GeoLocation'), {zoom: 12, center: uluru});
-        // The marker, positioned at Uluru
-        var marker = new google.maps.Marker({position: uluru, map: map});
     }
 }
