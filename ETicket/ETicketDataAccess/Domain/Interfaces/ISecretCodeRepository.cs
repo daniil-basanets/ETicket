14e1fdca3022a2abb5a8a0edff7af527e3e52f07
Daria Nazarenko <daryan15646@gmail.com>
2020-05-26 11:00:26 +0300

    Edit method name with Async.

--- ETicket/ETicketDataAccess/Domain/Interfaces/ISecretCodeRepository.cs
+++ ETicket/ETicketDataAccess/Domain/Interfaces/ISecretCodeRepository.cs
@@ -8,7 +8,7 @@ namespace ETicket.DataAccess.Domain.Interfaces
 {
     public interface ISecretCodeRepository
     {
-        public Task<SecretCode> Get(string code, string email);
+        public Task<SecretCode> GetAsync(string code, string email);
         public void RemoveRange(string email);
         public void Add(SecretCode code);
         public int Count(string email);
