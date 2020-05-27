915a903a87b9c009c5e15ccd66b51936c7437116
Anton Kalashnikov <kalash@softserve.com>
2020-05-27 22:01:26 +0300

    Add GetUsersPage method and maps DataTablePage

--- ETicket/ETicket.ApplicationServices/Mapping/TicketMapperProfile.cs
+++ ETicket/ETicket.ApplicationServices/Mapping/TicketMapperProfile.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using AutoMapper;
 using ETicket.ApplicationServices.DTOs;
+using ETicket.ApplicationServices.Services.PagingServices.Models;
 using ETicket.DataAccess.Domain.Entities;
 
 namespace ETicket.ApplicationServices.Mapping
@@ -16,13 +17,15 @@ namespace ETicket.ApplicationServices.Mapping
                        .ForMember(d => d.UserName, d => d.MapFrom(s => s.User.FirstName + ' ' + s.User.LastName))
                        .ForMember(d => d.Areas, d => d.MapFrom(s => s.TicketArea.Select(x => new KeyValuePair<int, string>(x.Area.Id, x.Area.Name))))
                        .ReverseMap();
+            CreateMap<DataTablePage<Ticket>, DataTablePage<TicketDto>>();
 
             CreateMap<TicketTypeDto, TicketType>().ReverseMap();
+            
             CreateMap<TicketVerificationDto, TicketVerification>()
                 .ReverseMap()
                 .ForMember(d => d.TransportNumber, d => d.MapFrom(x => x.Transport.Number))
                 .ForMember(d => d.StationName, d => d.MapFrom(x => x.Station.Name));
-
+            CreateMap<DataTablePage<TicketVerification>, DataTablePage<TicketVerificationDto>>();
         }
     }
 }
