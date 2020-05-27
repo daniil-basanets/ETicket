2dc991081ba898fe483711d658c03c93013ac38e
daniil-basanets <daniil.basanets@gmail.com>
2020-05-27 18:58:15 +0300

    254: Change BaseApiController method

--- ETicket/ETicketWebAPI/Controllers/BaseAPIController.cs
+++ ETicket/ETicketWebAPI/Controllers/BaseAPIController.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace ETicket.WebAPI.Controllers
 {
     public class BaseAPIController : ControllerBase
     {
-        protected virtual string GetJson(object dtoObject)
+        protected virtual IActionResult Json(object dtoObject, int statusCode = StatusCodes.Status200OK)
         {
-            return JsonConvert.SerializeObject(dtoObject, Formatting.Indented);
+            var jsonString = JsonConvert.SerializeObject(dtoObject, Formatting.Indented);
+            var result = new ObjectResult(jsonString)
+            {
+                StatusCode = statusCode
+            };
+
+            return result;
         }
     }
 }
