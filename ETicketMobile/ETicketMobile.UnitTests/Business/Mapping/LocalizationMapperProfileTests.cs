a013a34fbf81f8d2ec44244ee7b7e976b34f2347
Anton <anton.ukrainets@gmail.com>
2020-05-29 22:46:11 +0300

    Add mappers tests.

---
+++ ETicketMobile/ETicketMobile.UnitTests/Business/Mapping/LocalizationMapperProfileTests.cs
@@ -0,0 +1,37 @@
+﻿using ETicketMobile.Business.Mapping;
+using ETicketMobile.Data.Entities;
+using Xunit;
+
+namespace ETicketMobile.UnitTests.Business.Mapping
+{
+    public class LocalizationMapperProfileTests
+    {
+        [Fact]
+        public void MapEntityLocalizationToLocalization()
+        {
+            // Arrange
+            var entityLocalization = new Localization { Culture = "ru-RU" };
+
+            // Act
+            var localization = AutoMapperConfiguration.Mapper
+                    .Map<Localization, ETicketMobile.Business.Model.UserAccount.Localization>(entityLocalization);
+
+            // Assert
+            Assert.Equal(entityLocalization.Culture, localization.Culture);
+        }
+
+        [Fact]
+        public void MapLocalizationToEntityLocalization()
+        {
+            // Arrange
+            var localization = new ETicketMobile.Business.Model.UserAccount.Localization { Culture = "ru-RU" };
+
+            // Act
+            var entityLocalization = AutoMapperConfiguration.Mapper
+                    .Map<ETicketMobile.Business.Model.UserAccount.Localization, Localization>(localization);
+
+            // Assert
+            Assert.Equal(localization.Culture, entityLocalization.Culture);
+        }
+    }
+}
---
+++ ETicketMobile/ETicketMobile.UnitTests/Business/Mapping/TokenMapperProfileTests.cs
@@ -0,0 +1,46 @@
+﻿using ETicketMobile.Business.Mapping;
+using ETicketMobile.Data.Entities;
+using ETicketMobile.WebAccess.DTO;
+using Xunit;
+
+namespace ETicketMobile.UnitTests.Business.Mapping
+{
+    public class TokenMapperProfileTests
+    {
+        [Fact]
+        public void MapTokenDtoToToken()
+        {
+            // Arrange
+            var tokenDto = new TokenDto
+            {
+                AcessJwtToken = "AccessToken",
+                RefreshJwtToken = "RefreshToken"
+            };
+
+            // Act
+            var token = AutoMapperConfiguration.Mapper.Map<TokenDto, Token>(tokenDto);
+
+            // Assert
+            Assert.Equal(tokenDto.AcessJwtToken, token.AcessJwtToken);
+            Assert.Equal(tokenDto.RefreshJwtToken, token.RefreshJwtToken);
+        }
+
+        [Fact]
+        public void MapTokenToTokenDto()
+        {
+            // Arrange
+            var token = new Token
+            {
+                AcessJwtToken = "AccessToken",
+                RefreshJwtToken = "RefreshToken"
+            };
+
+            // Act
+            var tokenDto = AutoMapperConfiguration.Mapper.Map<Token, TokenDto>(token);
+
+            // Assert
+            Assert.Equal(token.AcessJwtToken, tokenDto.AcessJwtToken);
+            Assert.Equal(token.RefreshJwtToken, tokenDto.RefreshJwtToken);
+        }
+    }
+}
