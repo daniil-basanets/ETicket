36af7b8c03880677ad98f0500832e359c1b48038
Anton <anton.ukrainets@gmail.com>
2020-05-29 22:12:29 +0300

    Add Entity tests.

---
+++ ETicketMobile/ETicketMobile.UnitTests/Data/Entities/SettingsTests.cs
@@ -0,0 +1,30 @@
+﻿using ETicketMobile.Data.Entities;
+using Xunit;
+
+namespace ETicketMobile.UnitTests.Data.Entities
+{
+    public class SettingsTests
+    {
+        [Fact]
+        public void CreateInstance()
+        {
+            // Arrange
+            var id = 0;
+            var name = "Name";
+            var value = "Value";
+
+            // Act
+            var setting = new Setting
+            {
+                Id = id,
+                Name = name,
+                Value = value
+            };
+
+            // Assert
+            Assert.Equal(id, setting.Id);
+            Assert.Equal(name, setting.Name);
+            Assert.Equal(value, setting.Value);
+        }
+    }
+}
