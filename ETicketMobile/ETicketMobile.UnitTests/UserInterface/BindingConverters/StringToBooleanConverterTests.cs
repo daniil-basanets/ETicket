2c954413f24fdfb6462c35f6d3f45eaad449483e
Anton <anton.ukrainets@gmail.com>
2020-05-30 11:04:10 +0300

    Add binding converters tests.

---
+++ ETicketMobile/ETicketMobile.UnitTests/UserInterface/BindingConverters/StringToBooleanConverterTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ETicketMobile.UserInterface.BindingConverters;
+using Xunit;
+
+namespace ETicketMobile.UnitTests.UserInterface.BindingConverters
+{
+    public class StringToBooleanConverterTests
+    {
+        [Theory]
+        [InlineData("abc")]
+        [InlineData("123")]
+        public void Convert_Positive(string value)
+        {
+            // Arrange
+            var stringToBooleanConverter = new StringToBooleanConverter();
+
+            // Act
+            var actualValue = (bool)stringToBooleanConverter.Convert(value, null, null, null);
+
+            // Assert
+            Assert.True(actualValue);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Convert_Negative(string value)
+        {
+            // Arrange
+            var stringToBooleanConverter = new StringToBooleanConverter();
+
+            // Act
+            var actualValue = (bool)stringToBooleanConverter.Convert(value, null, null, null);
+
+            // Assert
+            Assert.False(actualValue);
+        }
+
+        [Fact]
+        public void ConvertBack()
+        {
+            // Arrange
+            var stringToColorConverter = new StringToBooleanConverter();
+
+            // Assert
+            Assert.Throws<NotImplementedException>(() => stringToColorConverter.ConvertBack(null, null, null, null));
+        }
+    }
+}
---
+++ ETicketMobile/ETicketMobile.UnitTests/UserInterface/BindingConverters/StringToColorConverterTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ETicketMobile.UserInterface.BindingConverters;
+using Xamarin.Forms;
+using Xunit;
+
+namespace ETicketMobile.UnitTests.UserInterface.BindingConverters
+{
+    public class StringToColorConverterTests
+    {
+        [Theory]
+        [InlineData("abc")]
+        [InlineData("123")]
+        public void Convert_Positive(string value)
+        {
+            // Arrange
+            var expectedValue = Color.Red;
+            var stringToColorConverter = new StringToColorConverter();
+
+            // Act
+            var actualValue = (Color)stringToColorConverter.Convert(value, null, null, null);
+
+            // Assert
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Convert_Negative(string value)
+        {
+            // Arrange
+            var expectedValue = Color.Default;
+            var stringToColorConverter = new StringToColorConverter();
+
+            // Act
+            var actualValue = (Color)stringToColorConverter.Convert(value, null, null, null);
+
+            // Assert
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void ConvertBack()
+        {
+            // Arrange
+            var stringToColorConverter = new StringToColorConverter();
+
+            // Assert
+            Assert.Throws<NotImplementedException>(() => stringToColorConverter.ConvertBack(null, null, null, null));
+        }
+    }
+}
