eb3d3879ce7259e69101cf4cd86dc4a13f67329c
Anton <anton.ukrainets@gmail.com>
2020-06-03 00:20:24 +0300

    Add LoginViewModelTests.

---
+++ ETicketMobile/ETicketMobile.UnitTests/Portable/ViewModels/Login/LoginViewModelTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicketMobile.UnitTests.Portable.ViewModels.Login
+{
+    public class LoginViewModelTests
+    {
+
+    }
+}
