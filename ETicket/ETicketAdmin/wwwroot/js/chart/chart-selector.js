882726942afc43361170626fd642816b12bbe815
AleksandrKar7 <AleksandrKar7@gmail.com>
2020-06-02 14:15:08 +0300

    Add border for div charts and message when there are no charts selected

--- ETicket/ETicketAdmin/wwwroot/js/chart/chart-selector.js
+++ ETicket/ETicketAdmin/wwwroot/js/chart/chart-selector.js
@@ -1,15 +1,28 @@
-﻿function ManageChart(checkBox, chartURL, chartDivId) {
+﻿var activeCharts = [];
+
+function ManageChart(checkBox, chartURL, chartDivId) {
     var isOn = checkBox.checked;
     if (isOn) {
         $.ajax({
             type: "GET",
             url: chartURL,
             success: function (result) {
+                activeCharts.push(chartDivId);
                 $(chartDivId).html(result);
+                $("#empty-chart-list").attr("hidden", "true");
             },
         });
     }
     else {
+        var index = activeCharts.indexOf(chartDivId);
+        if (index > -1) {
+            activeCharts.splice(index, 1);
+        }
+        if (activeCharts.length == 0) {
+            $("#empty-chart-list").removeAttr("hidden");
+        }
+
         $(chartDivId).html("");
     }
 }
+
