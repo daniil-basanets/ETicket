f98694f064056f582ecb2cb2f08e088540ea86bc
Anton <anton.ukrainets@gmail.com>
2020-06-04 21:26:41 +0300

    Add LocalTokenService.

---
+++ ETicketMobile/ETicketMobile.DataAccess/Services/LocalTokenService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using ETicketMobile.Data.Entities;
+using ETicketMobile.DataAccess.LocalAPI.Interfaces;
+using ETicketMobile.DataAccess.Services.Interfaces;
+
+namespace ETicketMobile.DataAccess.Services
+{
+    public class LocalTokenService : ILocalTokenService
+    {
+        #region Fields
+
+        private readonly ILocalApi localApi;
+
+        #endregion
+
+        public LocalTokenService(ILocalApi localApi)
+        {
+            this.localApi = localApi
+                ?? throw new ArgumentNullException(nameof(localApi));
+        }
+
+        public Task AddAsync(Token token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetAccessTokenAsync()
+        {
+            var token = await localApi.GetTokenAsync();
+
+            return token.AcessJwtToken;
+        }
+
+        public async Task<string> GetReshreshTokenAsync()
+        {
+            var token = await localApi.GetTokenAsync();
+
+            return token.RefreshJwtToken;
+        }
+    }
+}
