d2f7d53adeae8b89e805009fcc2554296bf3bc22
daniil-basanets <daniil.basanets@gmail.com>
2020-06-08 13:24:40 +0300

    Add autorization to swagger

--- ETicket/ETicketWebAPI/Startup.cs
+++ ETicket/ETicketWebAPI/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using ETicket.ApplicationServices.Logger;
 using ETicket.ApplicationServices.Services;
 using ETicket.ApplicationServices.Services.DocumentTypes;
@@ -65,7 +66,7 @@ namespace ETicket.WebAPI
                .AddEntityFrameworkStores<ETicketDataContext>()
                .AddDefaultTokenProviders()
                .AddTokenProvider(AuthOptions.ISSUER, typeof(DataProtectorTokenProvider<IdentityUser>));
-            
+
             services.AddIdentityCore<IdentityUser>(o =>
             {
                 o.Password.RequireDigit = false;
@@ -111,6 +112,35 @@ namespace ETicket.WebAPI
             {
                 c.EnableAnnotations();
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ETicket API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme.<br>" +
+                                  "Use POST '/api/authentication/token' to GET Bearer token.<br>" +
+                                  "Enter 'Bearer' [space] and then your token in the text input below.<br>" +
+                                  "Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+
             });
         }
 
